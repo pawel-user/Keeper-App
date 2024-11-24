ea58a8b5e98b62f198265358ae982d1823a4ad62
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2024-11-24 17:31:08 +0100

    Added front-end Keeper App built from React components

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles-link.css";  // import "./../public/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
